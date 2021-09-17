78b2101c049512a37cbfe7aab93f6f2f857d0fd3
CLJ <vine9151@gmail.com>
2021-09-17 17:57:45 +0900

    first commit

---
+++ ATmega328P/ATmega328P/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ATmega328P.cpp
+ *
+ * Created: 2021-09-17 17:42:08
+ * Author : Eira
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
